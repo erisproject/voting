b62e25f1767f1459bcf18c9453f2adc112d283d4
Jason Rhinelander <jason@imaginary.ca>
2013-09-15 17:19:25 -0400

    Added command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@
 #include <cmath>
 #include <ctime>
 #include <chrono>
+#include <vector>
+#include <algorithm>
+#include <tclap/CmdLine.h>
 
 using namespace voting;
 using namespace std::chrono;
@@ -68,7 +71,9 @@ void voterHist(const Eris<Simulation> &sim) {
     }
     for (auto &count : party_bins) {
         std::cout << (
-                count >= 3 ? "█" :
+                count > 4 ? std::to_string(count) :
+                count == 4 ? "█" :
+                count == 3 ? "▓" :
                 count == 2 ? "▒" :
                 count == 1 ? "░" :
                              " ");
@@ -76,21 +81,133 @@ void voterHist(const Eris<Simulation> &sim) {
     std::cout << "\n\n";
 }
 
-int main() {
+enum voter_dist { dist_even, dist_uniform, dist_beta22, dist_beta55 };
+struct prog_params {
+    unsigned int voters = 999;
+    unsigned int parties = 3;
+    unsigned int iterations = 1000;
+    double friend_prob = 0.01;
+    double influence_prob = 0.1;
+    double drift = 0.05;
+    bool influence_all = false;
+    bool constr_parties = false;
+    voter_dist vdist = dist_beta55;
+};
+template <class T, class = typename std::enable_if<std::is_arithmetic<T>::value>::type>
+class RangeConstraint : public TCLAP::Constraint<T> {
+    public:
+        RangeConstraint(T min) : min_(min), no_max_(true) {}
+        RangeConstraint(T min, T max) : min_(min), max_(max) {}
+        virtual std::string description() const override {
+            if (max_ == std::numeric_limits<double>::infinity())
+                return "Value must be at least " + std::to_string(min_);
+            else
+                return "Value must be between " + std::to_string(min_) + " and " + std::to_string(max_);
+        }
+        virtual std::string shortID() const override {
+            if (no_max_)
+                return "v >= " + std::to_string(min_);
+            else
+                return std::to_string(min_) + " <= v <= " + std::to_string(max_);
+        }
+        virtual bool check(const T &val) const override {
+            return val >= min_ and val <= max_;
+        }
+    private:
+        T min_;
+        T max_ = std::numeric_limits<T>::has_infinity ? std::numeric_limits<T>::infinity() : std::numeric_limits<T>::max();
+        bool no_max_ = false;
+};
+
+prog_params parseCmdArgs(int argc, char **argv) {
+    prog_params p;
+
+    try {
+        TCLAP::CmdLine cmd("Command description message", ' ', "0.0.1");
+        RangeConstraint<unsigned int> partiesConstr(1, 10);
+        TCLAP::ValueArg<unsigned int> partiesArg("p", "parties", "Number of parties to create", false, p.parties,
+                &partiesConstr, cmd);
+
+        TCLAP::SwitchArg constrArg("c", "constrained", "Constrain parties to +/- 0.5 of initial location.",
+                cmd, p.constr_parties);
+
+        RangeConstraint<unsigned int> votersConstr(1);
+        TCLAP::ValueArg<unsigned int> votersArg("v", "voters", "Number of voters to create", false, p.voters,
+                &votersConstr, cmd);
+
+        std::vector<std::string> dists;
+        dists.push_back("even");
+        dists.push_back("uniform");
+        dists.push_back("beta22");
+        dists.push_back("beta55");
+        TCLAP::ValuesConstraint<std::string> distValues(dists);
+        TCLAP::ValueArg<std::string> distArg("d", "voter-distribution", "Distribution of voters: even spreads voters evenly over the range; uniform uses a random uniform distribution; beta22 use a beta(2,2) distribution; beta55 uses a beta(5,5) distribution.", false,
+                p.voters == dist_even ? "even" : p.voters == dist_uniform ? "uniform" : p.voters == dist_beta22 ? "beta22" : "beta55",
+                &distValues, cmd);
+
+        RangeConstraint<unsigned int> iterConstr(1);
+        TCLAP::ValueArg<unsigned int> iterArg("i", "iterations", "Number of simulation iterations", false, p.iterations,
+                &iterConstr, cmd);
+
+        RangeConstraint<double> friendConstr(0, 1);
+        RangeConstraint<double> inflConstr(0, 1);
+        RangeConstraint<double> driftConstr(0, 2);
+
+        TCLAP::ValueArg<double> friendArg("F", "friend-probability", "The probability of any voter being a friend of any other voter",
+                false, p.friend_prob, &friendConstr, cmd);
+        TCLAP::ValueArg<double> inflArg("I", "influence-probability", "The probability that a voter will attempt to exhert influence on each friend",
+                false, p.influence_prob, &inflConstr, cmd);
+        TCLAP::ValueArg<double> driftArg("D", "drift-rate", "The speed at which influenced friends drift back to their initial location",
+                false, p.drift, &driftConstr, cmd);
+
+        TCLAP::SwitchArg inflAllArg("A", "influence-all", "If provided, voters attempt to exhert influence on their entire set of friends at once, with probability given by -I.  The default "
+                "behaviour attempts to exhert influence on each voter with the probability given by -I.",
+                cmd, p.influence_all);
+
+
+        cmd.parse(argc, argv);
+
+        p.parties = partiesArg.getValue();
+        p.constr_parties = constrArg.getValue();
+        p.voters = votersArg.getValue();
+        p.vdist = distArg.getValue() == "even" ? dist_even : distArg.getValue() == "uniform" ? dist_uniform :
+            distArg.getValue() == "beta22" ? dist_beta22 : dist_beta55;
+        p.friend_prob = friendArg.getValue();
+        p.influence_prob = inflArg.getValue();
+        p.influence_all = inflAllArg.getValue();
+        p.drift = driftArg.getValue();
+        p.iterations = iterArg.getValue();
+
+        return p;
+    }
+    catch (TCLAP::ArgException &e) {
+        std::cerr << "Error: " << e.error() << " for arg " << e.argId() << "\n";
+        throw;
+    }
+}
+
+int main(int argc, char **argv) {
+
+    auto params = parseCmdArgs(argc, argv);
+    std::string run_details = std::to_string(params.parties) + " parties (" +
+        (params.constr_parties ? "" : "un") + "constrained), " +
+        std::to_string(params.voters) + " voters (initial dist: " +
+        (params.vdist == dist_even ? "even" : params.vdist == dist_uniform ? "uniform" :
+         params.vdist == dist_beta22 ? "beta(2,2)" : "beta(5,5)") +
+        "); F=" + std::to_string(params.friend_prob) + "; I=" + std::to_string(params.influence_prob) +
+        "; A=" + std::to_string(params.influence_all) + "; D=" + std::to_string(params.drift);
+
     Eris<Simulation> sim;
 
-    sim->createAgent<Party>(-0.5, -1, 1);
-    sim->createAgent<Party>(0.5, -1, 1);
-/*    auto p_left  = sim->createAgent<Party>(-0.5, -1, 0);
-    auto p_right = sim->createAgent<Party>(0.5, 0, 1);
-    auto p_centre = sim->createAgent<Party>(0, 0.5, -0.5);*/
-
-/*    sim->createInterOpt<PartyMover>(p_left,   1.0/32, 4, 1.0/1048576, 3, 1);
-    sim->createInterOpt<PartyMover>(p_centre, 1.0/32, 4, 1.0/1048576, 3, 2);
-    sim->createInterOpt<PartyMover>(p_right,  1.0/32, 4, 1.0/1048576, 3, 0);*/
-
-    //auto rng = eris::Random::rng();
-    //std::uniform_real_distribution<double> runif(-1, 1);
+    for (int i = 1; i <= params.parties; i++) {
+        double pos = -1.0 + i*2.0 / (params.parties+1);
+        double left_con = -1.0, right_con = 1.0;
+        if (params.constr_parties) {
+            left_con = std::max(pos - 0.5, -1.0);
+            right_con = std::min(pos + 0.5, 1.0);
+        }
+        sim->createAgent<Party>(pos, left_con, right_con);
+    }
 
     // Candidate num voters, based on having single element quantiles up to (only the smallest 5 listed for each)
     // 24: 5354228879
@@ -106,48 +223,46 @@ int main() {
     //  4: 11, 23, 35, 47, 71
     //  3: 5, 17, 29, 41, 53
 
-    int num_voters = 199;
-    //int num_voters = 2519;
-    //int num_voters = 5;
-
     // The probability of any random voter v2 being a friend of v1.
-    double friend_prob = 0.1;
+    // FIXME: cmd-line args for all of these
+/*    double friend_prob = 0.1;
     double influence_prob = 0.1;
-    double drift = 0.05;
+    double drift = 0.05;*/
     // True: influence everyone or no one with prob (influence_prob); false:
     // influence each friend with prob (influence_prob).
     bool influence_all = false;
 
-    // Uniform initial positioner:
-    auto positioner_unif = [](int i, int num_voters) -> double { return -1 + 2 * i / (num_voters - 1.0); };
 
-    // Random uniform initial positioner:
-    std::uniform_real_distribution<double> runif(-1.0, 1.0);
-    auto positioner_runif = [&runif](int i, int num_voters) -> double { return runif(Random::rng()); };
+    auto rng = Random::rng();
+    std::function<double(int)> positioner;
+    if (params.vdist == dist_even) {
+        // Uniform initial positioner:
+        positioner = [&](int i) -> double { return -1 + 2 * i / (params.voters - 1.0); };
+    }
+    else if (params.vdist == dist_uniform) {
+        // Random uniform initial positioner:
+        std::uniform_real_distribution<double> runif(-1.0, 1.0);
+        positioner = [&](int i) -> double { return runif(rng); };
+    }
+    else {
+        // Random beta(a,b) distributed (rescaled from [0,1] to [-1,1])
+        double beta_a = 5.0, beta_b = 5.0;
+        std::gamma_distribution<double> gamma_a(beta_a, 1), gamma_b(beta_b, 1);
+        positioner = [&](int i) -> double {
+            double x = gamma_a(rng);
+            double y = gamma_b(rng);
+            return -1 + 2 * (x / (x + y));
+        };
+    }
 
-    // Random beta(a,b) distributed (rescaled from [0,1] to [-1,1])
-    double beta_a = 5.0, beta_b = 5.0;
-    std::gamma_distribution<double> gamma_a(beta_a, 1), gamma_b(beta_b, 1);
-    auto positioner_rbeta = [&gamma_a, &gamma_b](int i, int num_voters) -> double {
-        double x = gamma_a(Random::rng());
-        double y = gamma_b(Random::rng());
-        return -1 + 2 * (x / (x + y));
-    };
 
-
-    std::function<double(int, int)> positioner;
-    // Which positioner to use (done this way to avoid unused variable warnings/errors)
-    if (1) positioner = positioner_unif;
-    else if (0) positioner = positioner_runif;
-    else positioner = positioner_rbeta;
-
-    for (int i = 0; i < num_voters; i++) {
-        auto voter = sim->createAgent<Voter>(positioner(i, num_voters), -1, 1);
-        sim->createInterOpt<Influence>(voter, influence_prob, drift, influence_all);
+    for (int i = 0; i < params.voters; i++) {
+        auto voter = sim->createAgent<Voter>(positioner(i), -1, 1);
+        sim->createInterOpt<Influence>(voter, params.influence_prob, params.drift, influence_all);
 //        sim->createAgent<Voter>(runif(rng), -1, 1);
     }
 
-    std::bernoulli_distribution friend_flip(friend_prob);
+    std::bernoulli_distribution friend_flip(params.friend_prob);
     // Create some friendships
     for (auto &v1 : sim->agentFilter<Voter>()) {
         for (auto &v2 : sim->agentFilter<Voter>()) {
@@ -174,10 +289,10 @@ int main() {
     voterHist(sim);
 
     std::cout << "\e[1;0H"; // Position at line 0, col 0
-    std::cout << "Running!\n";
+    std::cout << "Running (" << run_details << ")\n";
 //    if (0)
     auto last = steady_clock::now();
-    for (int i = 1; i <= 1000; i++) {
+    for (int i = 1; i <= params.iterations; i++) {
         sim->run();
         auto now = steady_clock::now();
         double speed = 1.0/duration_cast<duration<double>>(now - last).count();
@@ -196,9 +311,10 @@ int main() {
 
     std::cout << "\e[?12;25h"; // Restore cursor (from `tput cvvis`)
 
-    std::cout << "Parties:\n    id   @ position\n    ----   ---------\n";
+    std::cout << "Parties:\n    id   @  position   constraint\n    ----   ---------   ---------------------\n";
     for (auto &p : sim->agentFilter<Party>()) {
-        printf("    %-4ld @ %9.6f\n", p.first, p.second->position()[0]);
+        printf("    %-4ld @ %9.6f   [%9.6f,%9.6f]\n", p.first, p.second->position()[0],
+                p.second->lowerBound()[0], p.second->upperBound()[0]);
     }
     /*std::cout << "Voters:\n";
     int cols = 6, col = 0;
