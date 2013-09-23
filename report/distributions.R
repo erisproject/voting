1f985ae195d27bbc79df8bacda763dd8b9709648
Jason Rhinelander <jason@imaginary.ca>
2013-09-23 18:22:55 -0400

    Report updates

--- report/distributions.R
+++ report/distributions.R
@@ -5,7 +5,7 @@ unif <- runif(999, -1, 1);
 beta22 <- rbeta(999, 2, 2) * 2 - 1;
 beta55 <- rbeta(999, 5, 5) * 2 - 1;
 
-pdf("dists.pdf", width=3, height=4);
+pdf("dists.pdf", width=3.25, height=3.5);
 par("mar", c(5.1,4.1,4.1,5));
 hist(even, -25:25/25, ylim=c(0,1.5), freq=FALSE);
 hist(unif, -25:25/25, ylim=c(0,1.5), freq=FALSE);
